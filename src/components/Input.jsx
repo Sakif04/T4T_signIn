c203a2d74fae46171ad515252d20dd6b524c604a
Sakif Rafid <sakifrafid04@gmail.com>
2022-03-21 21:12:24 +0600

    T4T design complete

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 export default function Input({item}){
-
+const [input,setInput]=useState("");
+    const changeInput=(e)=>{
+        setInput(e.target.value)
+    }
     return(  
-        <input  name={item} type={item} id={`input-${item}`} className="input"
+        <input value={input} onChange={changeInput}  name={item} type={item} id={`input-${item}`} className="input" 
         placeholder={item=="email"?`Enter your ${item} address`:`Enter your ${item}`} />
         
     )
--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,4 +1,6 @@
 import logo from "./../img/logo.svg";
+import fbIcon from "./../img/fb-icon.svg";
+import googleIcon from "./../img/google-icon.svg"
 import Input from "./Input";
 export default function SignInForm(){
 
@@ -9,6 +11,18 @@ export default function SignInForm(){
 
             <Input item='email'/>
             <Input item="password"/>
+            <button className="btn">Log in</button>
+            <h4 className="or">or</h4>
+            <div className="icons">
+                <img src={fbIcon} alt="Facebook-icon" className="icon icon-fb" />
+                <img src={googleIcon} alt="Google-Icon" className="icon icon-google" />
+                
+             </div> 
+             
+        <h4 className="unregistered">Don't have an account?  a   <a href="#" className="link">Sign Up</a></h4>
+            
+
+            
         
 
     </form>
